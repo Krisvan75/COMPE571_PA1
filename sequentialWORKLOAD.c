2ffc2f1bd7dfbe1fa95efd0aafa6befc646177cc
krishna <krishna@krish.local>
2025-02-18 11:26:09 -0800

    SequentialWorkload added

---
+++ sequentialWORKLOAD.c
@@ -0,0 +1,27 @@
+//
+//  baselineWORKLOAD
+//
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include<time.h>
+
+
+int main(int argc, const char * argv[]) {
+    int inputNum = atoi(argv[1]);
+    long long Sum=0;
+    
+    clock_t Start, Stop;
+    Start = clock();
+    for(int i=1; i<inputNum; i++){
+        
+        Sum += i;
+        
+    }
+    Stop = clock();
+    printf("Sum: %lld \n",Sum);
+    double timer = (double)(Stop - Start)/CLOCKS_PER_SEC;
+    printf("Time Elapsed: %0.20f", timer);
+    
+}
