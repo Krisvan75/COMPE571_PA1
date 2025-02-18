43e07a9b0d161517e11ee8878ec2f228b3c0bafb
krisvan75 <krisvanga75@gmail.com>
2025-02-18 13:32:20 -0800

    Complete implementations for multitasking and multithreading

--- Multitasking.c
+++ Multitasking.c
@@ -8,14 +8,14 @@ Multitasking implementation of WORKLOAD
 #include <unistd.h>
 #include <sys/wait.h>
 
+long long totalSum = 0;
 
 
 int main(int argc, const char *argv[]){
     long long N = atoll(argv[1]);
     int numProcesses = atoi(argv[2]);
     
-    
-    long long chunk = N/numProcesses;
+    long long Nrange = N/numProcesses;
     int pipes[numProcesses][2];
     pid_t PIDs[numProcesses];
     clock_t Start, Stop;
@@ -25,27 +25,27 @@ int main(int argc, const char *argv[]){
         pipe(pipes[i]);
         PIDs[i] = fork();
         if(PIDs[i] == 0){
-            long long start = i*chunk;
-            long long end = (i==numProcesses-1) ? N : (i+1)*chunk;
-            long long local_sum = 0;
+            long long localSum = 0;
+            long long start = i*Nrange;
+            long long end = (i==numProcesses-1) ? N : (i+1)*Nrange;
             for(long long j=start; j<end; j++){
-                local_sum += j;
+                localSum += j;
             }
-            write(pipes[i][1], &local_sum, sizeof(local_sum));
+            write(pipes[i][1], &localSum, sizeof(localSum));
             close(pipes[i][1]);
             exit(0);
         }
     }
-    long long total_sum = 0;
+
     for(int i=0; i<numProcesses; i++){
         long long local_sum;
         read(pipes[i][0], &local_sum, sizeof(local_sum));
         close(pipes[i][0]);
-        total_sum += local_sum;
+        totalSum += local_sum;
     }
     Stop = clock();
     double timer = (double)(Stop - Start)/CLOCKS_PER_SEC;
-    printf("Sum: %lld\n", total_sum);
+    printf("Sum: %lld\n", totalSum);
     printf("Time Elapsed: %0.13f\n", timer);
     return 0;
 }
--- multithreading.c
+++ multithreading.c
@@ -12,7 +12,7 @@ Multithreading implementation of WORKLOAD
 long long total_sum = 0;
 
 
-void* Workload(void *args){
+void *Workload(void *args){
     long long *thread_args = (long long *)args;
     long long local_sum = 0;
     for(long long i=thread_args[0]; i<thread_args[1]; i++){
@@ -20,26 +20,27 @@ void* Workload(void *args){
     }
     thread_args[2] = local_sum;
 
+    return NULL;
 
 }
 
 
 
 int main(int argc, const char * argv[]) {
-    unsigned long long inputNum = atoll(argv[1]);
+    unsigned long long N = atoll(argv[1]);
     int numThreads = atoi(argv[2]);
     // Create an array of threads
     pthread_t threads[numThreads];
     clock_t Start, Stop;
 
     unsigned long long thread_args[numThreads][3]; //Start index, stop index, and local sum for each thread
-    unsigned long long chunk = inputNum/numThreads;
+    unsigned long long Nrange = N/numThreads;
 
 
     Start = clock();
     for(int i=0; i<numThreads; i++){
-        thread_args[i][0] = i * chunk;
-        thread_args[i][1] = (i==numThreads-1) ? inputNum : (i+1) * chunk;
+        thread_args[i][0] = i * Nrange;
+        thread_args[i][1] = (i==numThreads-1) ? N : (i+1) * Nrange;
         pthread_create(&threads[i], NULL, Workload, (void *)&thread_args[i]);   
     }
     for(int i=0; i<numThreads; i++){
